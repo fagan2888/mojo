31d38d05815b87c1d7fdb878f39aa1ec0063783a
nirsa <nirsa11@gmail.com>
2020-02-12 21:47:34 +0200

    wip

--- index.php
+++ index.php
@@ -19,8 +19,8 @@
 
   
 
-<script> let allData = <?php print_r($all);?>;</script> 
-<script> let fromDb = <?php print_r($coll);?>;</script>
+<script> let allData = <?php print_r($all);?>; let fromDb = <?php print_r($coll);?>;   </script> 
+
    
 <div class="container ">
 <h3>My Google Maps Demo</h3>
