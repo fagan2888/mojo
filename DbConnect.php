fb09f64a71a9213992b515dbb898453cadc98447
nirsa <nirsa11@gmail.com>
2020-02-12 21:56:15 +0200

    wip

--- DbConnect.php
+++ DbConnect.php
@@ -1,7 +1,7 @@
 <?php
 //connect to DB
 class Dbconnect{
-private $host = 'localhost';
+private $host = 'az1-ss17';
 private $dbname ='nirsaban_mojo';
 private $user = 'nirsaban_mojo';
 private $pass = '123456';
