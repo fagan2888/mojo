367f8188ca0dd7f2e263ce75e27e3af46cdca847
nirsa <nirsa11@gmail.com>
2020-02-13 20:51:09 +0200

    wip

--- php/DbConnect.php
+++ php/DbConnect.php
@@ -8,7 +8,6 @@ private $pass = '';
 public function connect(){
     try {
         $conn = new PDO('mysql:host=' .$this->host. ';dbname='.$this->dbname ,$this->user,$this->pass);
-       
         $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         return $conn;
       }catch(PDOException $e){
